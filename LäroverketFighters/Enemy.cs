1257fd81c717b8b264e30bc79cbcb19bf9f05b29
Brussan <Brussan@users.noreply.github.com>
2018-01-18 14:34:00 +0100

    Cleaned up program and added functionallity for enemy class

---
+++ LäroverketFighters/Enemy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LäroverketFighters
+{
+    class Enemy
+    {
+        int hp;
+        public int dmg;
+        public string name;
+        public bool isAlive = true;
+
+        public void Setup()
+        {
+            Random randomness = new Random();
+
+            hp = randomness.Next(10, 20);
+            dmg = randomness.Next(2, 6);
+
+            string[] namesToPick =
+            {
+                "Danne", //0
+                "Gunnar", //1
+                "Kristina", //2
+                "Branko", //3
+                "Hero",  //4
+                "Nicke", //5
+                "Nehmet", //6
+                "G4" //7
+            };
+            name = namesToPick[randomness.Next(0, namesToPick.Length)];
+         }//end of void setup
+
+        public void TakeDamage(int _damage)
+        {
+            hp -= _damage;
+
+            if (hp < 0)
+            {
+                isAlive = false;
+            }
+        }
+
+
+
+        public void Heal(int _healAmount)
+        {
+            hp += _healAmount;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(name + "'s HP:" + hp);
+        }
+
+    }//End of class
+}
--- LäroverketFighters/Properties/AssemblyInfo.cs
+++ LäroverketFighters/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("LäroverketFighters")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("LäroverketFighters")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b9384be7-cafd-455f-a303-6f29f6424351")]
+[assembly: Guid("d2398be2-23be-4aac-a506-814738279141")]
 
 // Version information for an assembly consists of the following four values:
 //
