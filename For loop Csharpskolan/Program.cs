5cf1f30c7ba5b19dc89ce487061fcefa68d1f9b5
Brussan <Brussan@users.noreply.github.com>
2018-01-18 15:12:49 +0100

    Foor loop exercises

---
+++ For loop Csharpskolan/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_loop_Csharpskolan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //övning 1
+            //for (int i = 0; i < 101; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //övning 2
+            //for (int i = 20; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+
+            //}
+
+
+
+            //övning 3
+            for (int i = 0; i < 52; i=i+2)
+            {
+                
+                Console.WriteLine(i);
+            }
+           
+            Console.ReadKey();
+        }
+    
+    }
+}
